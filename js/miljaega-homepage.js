dca6a312681dd202716ce8db4180420eb1e1276b
sarim hassan <sarimthegr8@gmail.com>
2025-07-28 16:23:16 +0500

    first commit

---
+++ js/miljaega-homepage.js
@@ -0,0 +1,19 @@
+document
+  .querySelector(".voice-search-btn")
+  .addEventListener("click", function () {
+    this.classList.toggle("text-red-500"); // Changes color when active
+    // Add your voice recognition logic here
+  });
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleBtn = document.getElementById("mobileMenuToggle");
+  const mobileMenu = document.getElementById("mobileMenu");
+
+  if (!toggleBtn || !mobileMenu) {
+    console.error("mobileMenuToggle or mobileMenu not found.");
+    return;
+  }
+
+  toggleBtn.addEventListener("click", function () {
+    mobileMenu.classList.toggle("hidden");
+  });
+});
